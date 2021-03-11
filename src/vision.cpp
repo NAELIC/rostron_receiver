2c5aeef5b77d7d29487b4c1e9d331ada6c58166e
Etienne Schmitz <contact@etienne-schmitz.com>
2021-03-11 22:23:09 +0100

    wip - add vision processing packet

--- src/vision.cpp
+++ src/vision.cpp
@@ -3,6 +3,8 @@
 #include "rostron_receiver/net/udp_receiver.h"
 #include "ssl_vision_wrapper.pb.h"
 
+#include "rostron_interfaces/msg/detection_frame.hpp"
+
 boost::asio::io_context io;
 
 class Vision : public rclcpp::Node
@@ -10,19 +12,39 @@ class Vision : public rclcpp::Node
 public:
   Vision() : Node("vision"),
              receiver_("224.5.23.2", "224.5.23.2", 10020, io,
-                       Vision::parseSimPacket)
+                       Vision::parseVisionPacket)
   {
+    publisher_ = this->create_publisher<rostron_interfaces::msg::DetectionFrame>("topic", 10);
   }
 
-  static void parseSimPacket(std::size_t length, std::array<char, 2048> data)
+  static void parseVisionPacket(std::size_t length, std::array<char, 2048> data)
   {
-    SSL_WrapperPacket sim_packet;
-    sim_packet.ParseFromArray(data.data(), length);
-    std::cout << "Detection : " << sim_packet.has_detection() << std::endl;
+    SSL_WrapperPacket vision_packet;
+    vision_packet.ParseFromArray(data.data(), length);
+    auto message = rostron_interfaces::msg::DetectionFrame();
+
+    if (vision_packet.has_detection())
+    {
+      auto &frame = vision_packet.detection();
+
+      for (auto &ball : frame.balls())
+      {
+        rostron_interfaces::msg::DetectionBall ball_msg;
+        ball_msg.confidence = ball.confidence();
+        ball_msg.pixel.set__x(ball.pixel_x());
+        ball_msg.pixel.set__y(ball.pixel_y());
+        ball_msg.position.set__x(ball.x());
+        ball_msg.position.set__y(ball.y());
+        message.balls.push_back(ball_msg);
+      }
+    }
+
+    // Vision::publisher_->publish(message);
   }
 
 private:
   UDPReceiver receiver_;
+  static rclcpp::Publisher<rostron_interfaces::msg::DetectionFrame>::SharedPtr publisher_;
 };
 
 int main(int argc, char **argv)
